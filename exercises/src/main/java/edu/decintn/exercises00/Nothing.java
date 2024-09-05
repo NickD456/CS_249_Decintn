222ac5e8ee25caa62e57d9936b3bf0076e7077b2
flip4567 <147349946+flip4567@users.noreply.github.com>
2024-09-05 15:11:10 -0400

    merged

--- exercises/src/main/java/edu/decintn/exercises00/Nothing.java
+++ exercises/src/main/java/edu/decintn/exercises00/Nothing.java
@@ -3,7 +3,7 @@ package edu.decintn.exercises00;
 import edu.decintn.exercises00.items.*;
 
 public class Nothing {
-    public static void main(String [] args) {
+    public static void main(String [] args ) {
         int x  = 5;
 
         Weapon j = new Weapon();
