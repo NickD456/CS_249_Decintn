026bb843f2260beec400177fcfc8b0fc694f1a19
flip4567 <147349946+flip4567@users.noreply.github.com>
2024-09-06 14:59:29 -0400

    assign01 done

---
+++ assign01/src/main/java/edu/decintn/assign01/Greetings.java
@@ -0,0 +1,9 @@
+package edu.decintn.assign01;
+
+public class Greetings {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Object Oriented Programming ");
+    }
+}
