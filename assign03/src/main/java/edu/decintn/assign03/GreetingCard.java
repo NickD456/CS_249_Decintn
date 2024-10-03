798ae5005b55bf5f52ab199aa3752b20338a4197
flip4567 <147349946+flip4567@users.noreply.github.com>
2024-10-03 15:36:25 -0400

    cont assign 3

--- assign03/src/main/java/edu/decintn/assign03/GreetingCard.java
+++ assign03/src/main/java/edu/decintn/assign03/GreetingCard.java
@@ -1,27 +1,44 @@
 package edu.decintn.assign03;
 
 public class GreetingCard {
+
+    private String[] otherLines;
+    private String[] lines;
+    private char boundaryChar;
+
     public GreetingCard(String [] lines, char boundaryChar)
     {
+        setLines(lines);
+        setBoundaryChar(boundaryChar);
 
     }
     public char getBoundaryChar()
     {
-
+        return boundaryChar;
     }
 
     public String getLines()
     {
+        StringBuilder sb = new StringBuilder();
+        int size = lines.length;
 
+        for(int i = 0; i < size; i++)
+        {
+            sb.append(lines[i]);
+            sb.append("/n");
+        }
+
+        return sb.toString();
     }
 
     public void setBoundaryChar(char boundaryChar)
     {
-
+        this.boundaryChar = boundaryChar;
     }
 
     public void setLines(String [] lines)
     {
+
 
     }
 
--- assign03/src/main/java/edu/decintn/assign03/Hallmark.java
+++ assign03/src/main/java/edu/decintn/assign03/Hallmark.java
@@ -15,11 +15,23 @@ public class Hallmark {
         String intString = input.nextLine();
         int lineCount = Integer.parseInt(intString);
 
+        String[] allLines = new String[lineCount];
+        System.out.println("Enter lines:");
+
+        for(int i=0; i<lineCount; i++)
+        {
+            allLines[i] = input.nextLine();
+        }
+
+        return new GreetingCard(allLines,bound);
     }
 
     public static void main(String [] args)
     {
-
+        Scanner input = new Scanner(System.in);
+        GreetingCard n = generateCard(input);
+        System.out.println("For any occasion:");
+        System.out.println(n);
     }
 
 
